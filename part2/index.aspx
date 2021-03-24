5744348f06656286f2a8cf7af76342140b1bf738
Ian Fanefjord <iafa@mercantec.dk>
2021-03-24 16:09:41 +0100

    login works

--- part2/index.aspx
+++ part2/index.aspx
@@ -5,11 +5,14 @@
     <div id="page">
         <div id="loginbox">
             <div id="subtitle">Login</div>
-            <div id="inputbox">
-                <div id="username">Username:<asp:TextBox class="loginfield" ID="TextBox1" runat="server"></asp:TextBox></div>
-                <div id="password">Password :<asp:TextBox class="loginfield" ID="TextBox2" runat="server"></asp:TextBox></div>
+            <div id="inputbox" style="padding-top:1vh;">
+                <div style="font-size:1.5vh;">Username:<asp:TextBox class="loginfield" ID="TextBox1" runat="server"></asp:TextBox></div>
+                <div style="font-size:1.5vh;">Password :<asp:TextBox class="loginfield" ID="TextBox2" runat="server"></asp:TextBox></div>
             </div>
-            <div id="submit"><asp:Button class="button" ID="Button1" runat="server" Text="Login" /></div>
+            <div id="submit"><asp:Button class="button" style="color:aliceblue;" ID="Button1" runat="server" Text="Login" OnClick="Button1_Click" /></div>
         </div>
     </div>
+    
+<asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:RegiConnectionString %>" SelectCommand="SELECT * FROM [Customer]"></asp:SqlDataSource>
+    
 </asp:Content>
--- part2/index.aspx.cs
+++ part2/index.aspx.cs
@@ -4,14 +4,52 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
 
 namespace part2
 {
-    public partial class index : System.Web.UI.Page
+     public partial class index : System.Web.UI.Page
     {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RegiConnectionString"].ConnectionString);
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            //SqlConnection con = new SqlConnection(conStr);
+            
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception er)
+            {
+                Response.Write("<script>alert('connection failed'+'"+er+"');</script>");
+            }
+            finally { conn.Close();}
+            
+        }
 
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            string userid = TextBox1.Text;
+            string password = TextBox2.Text;
+            SqlCommand cmd = new SqlCommand("select FirstName,Password from Customer where FirstName='" + TextBox1.Text + "'and password='" + TextBox2.Text + "'", conn);
+            //SqlDataAdapter da = new SqlDataAdapter(cmd);
+            //DataTable dt = new DataTable();
+            //da.Fill(dt);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read()) // using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                //Response.Write("<script>alert('"+ reader["FirstName"] + "');</script>");
+                Response.Redirect("~/Pages/ProductView.aspx");
+            }
+            else
+            {
+                Response.Write("<script>alert('Invalid Login please check username and password');</script>");
+            }
+            conn.Close();
         }
     }
 }
