3eb1e2ff0896c443d5658695fb4682f7ceb18560
Ian Fanefjord <iafa@mercantec.dk>
2021-03-25 15:09:06 +0100

    XML task done

--- part2/Pages/XMLDatafile.aspx
+++ part2/Pages/XMLDatafile.aspx
@@ -1,6 +1,15 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Pages/Site1.Master" AutoEventWireup="true" CodeBehind="XMLDatafile.aspx.cs" Inherits="part2.Pages.XMLDatafile" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    XML Data file
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">    
+    <div id="page">
+        <div id="loginbox" style="height:25vh;width:40vw;margin-left:15vw;">
+            <div id="subtitle" style="width:38vw;">Product View</div>
+            <div id="inputbox" style="padding-top:1vh;height:18vh;width:38vw;color:aliceblue;">
+    
+                <asp:GridView ID="GridView1" runat="server"></asp:GridView>   
+                <asp:Button class="button" ID="Button1" runat="server" Text="Show XML File" OnClick="Button1_Click" style="margin-top:1vh;"/>
+            </div>
+        </div>
+    </div>
 </asp:Content>
--- part2/Pages/XMLDatafile.aspx.cs
+++ part2/Pages/XMLDatafile.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +14,31 @@ namespace part2.Pages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RegiConnectionString"].ConnectionString);
+            try
+            {
+                conn.Open();
+                SqlDataAdapter sqlData = new SqlDataAdapter("select * from Employee", conn);
+                DataSet dataset = new DataSet();
 
+                sqlData.Fill(dataset, "Employee");
+                dataset.WriteXml(Server.MapPath("XMLFil.xml"));
+
+                dataset = new DataSet();
+                dataset.ReadXml(MapPath("~/Pages/XMLFil.xml"));
+                GridView1.DataSource = dataset;
+                GridView1.DataBind();
+            }
+            catch (Exception er)
+            {
+                Response.Write("<script>alert('connection failed'+'" + er + "');</script>");
+            }
+            finally { conn.Close(); }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Pages/XMLFil.xml");
         }
     }
 }
